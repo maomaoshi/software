3440cdabfecd58a906062d5dbd966bdf274b2b32
wawayu <15616011528@163.com>
2018-12-19 22:24:46 +0800

    数据库

---
+++ software.sql
@@ -0,0 +1,84 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50714
+Source Host           : localhost:3306
+Source Database       : software
+
+Target Server Type    : MYSQL
+Target Server Version : 50714
+File Encoding         : 65001
+
+Date: 2018-12-19 22:22:14
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for courses
+-- ----------------------------
+DROP TABLE IF EXISTS `courses`;
+CREATE TABLE `courses` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for students
+-- ----------------------------
+DROP TABLE IF EXISTS `students`;
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stu_id` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `teacher_id` int(11) DEFAULT NULL,
+  `teacher_work_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_teacher_id` (`teacher_id`),
+  KEY `FK_TEACHER_WORK_ID` (`teacher_work_id`),
+  KEY `stu_id` (`stu_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for student_score
+-- ----------------------------
+DROP TABLE IF EXISTS `student_score`;
+CREATE TABLE `student_score` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stu_id` varchar(11) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `score` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_STU_ID` (`stu_id`),
+  KEY `FK_COURSE_ID` (`course_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for teachers
+-- ----------------------------
+DROP TABLE IF EXISTS `teachers`;
+CREATE TABLE `teachers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `work_id` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `work_id` (`work_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for teacher_course
+-- ----------------------------
+DROP TABLE IF EXISTS `teacher_course`;
+CREATE TABLE `teacher_course` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `teacher_work_id` varchar(11) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+  `is_use` int(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_COURSE_ID` (`course_id`),
+  KEY `FK_WORK_ID` (`teacher_work_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
